c412fd2cd87a8a1ba773aa9daa9d04f87ad05670
Kaarthik <kaarthik.alagappan@gmail.com>
2021-01-17 22:15:13 -0500

    Adding helmet sections and updating favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,17 @@ import About from '../components/About'
 import Resume from "../components/Resume";
 import Home from "../components/Home";
 import Footer from "../components/Footer"
+import {Helmet} from "react-helmet";
+import favicon from "../../static/favicon.png"
 
 const Index = () => {
   return (
     <>
+    <Helmet>
+        <meta charSet="utf-8" />
+        <title>Kaarthik Alagappan</title>
+        <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+    </Helmet>
       <div className="static">
         <Home />
       </div>
